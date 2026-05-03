e68b5783305d15cb4644c8cc92756f2651b9a832
Arthur Tanaka <arthurtanaka.dev@gmail.com>
2026-05-03 13:14:08 -0300

    limpeza e tratamento de dados dos arquivos urls e views

--- API_GASTO/transactions/urls.py
+++ API_GASTO/transactions/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from rest_framework import routers
-from .views import TransactionViewSet, saldo, DashboardView
+from .views import TransactionViewSet, DashboardView
 
 router = routers.DefaultRouter()
 router.register('transactions', TransactionViewSet)
 
 urlpatterns = [
-    path('saldo/', saldo),
     path('dashboard/', DashboardView.as_view()),
 ] + router.urls
